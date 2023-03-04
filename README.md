a285346d0c2aaef0c68e78044f6ed83a83355481
Lê Thành Lộc <Thanhlocne246@gmail.com>
2023-03-04 19:56:21 +0700

    Set up project

---
+++ README.md
@@ -0,0 +1,9 @@
+express
+express-handlebars
+
+nodemon
+morgan
+node-sass
+
+npm  start để live
+npm run watch để cập nhật css
---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const { engine } = require('express-handlebars');
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+// HTTP logger
+app.use(morgan('combined'));
+
+// route
+app.get('/main', (req, res) => {
+  res.render('home');
+})
+
+// Template engine
+app.engine('hbs', engine({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'resources/views'))
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+})
